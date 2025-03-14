addb141c094b79131d19f404c7ba1f9ff8d74b93
Elliooot <Stephen900508@gmail.com>
2025-03-14 17:34:09 +0000

    Set STATIC_ROOT for production

--- personal_finance_tracker/settings.py
+++ personal_finance_tracker/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-6qbg#(up3qc%)r-($&npvm#xe-hfm1e_qr3mdm_(*_$%i_dezt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '.localhost', 'personal-finance-tracker-c2bhaad7ecevbtes.ukwest-01.azurewebsites.net']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', '.localhost', 'personal-finance-tracker-c2bhaad7ecevbtes.ukwest-01.azurewebsites.net']
 
 
 # Application definition
@@ -124,6 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
