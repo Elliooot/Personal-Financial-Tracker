3db037352b9a17ea621bbdb3dcb73637b427b817
Elliooot <Stephen900508@gmail.com>
2025-03-19 04:52:49 +0000

    Modify wsgi, .yml

--- personal_finance_tracker/wsgi.py
+++ personal_finance_tracker/wsgi.py
@@ -7,10 +7,22 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+from dotenv import load_dotenv
+
+load_dotenv()
+
+project_path = os.environ.get("DJANGO_PROJECT_PATH")
+venv_path = os.environ.get("VENV_PATH")
+
+sys.path.append(project_path)
+sys.path.append(venv_path)
+
+# poiting to the project settings
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "personal_finance_tracker.settings")
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_finance_tracker.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_finance_tracker.settings')
 
 application = get_wsgi_application()
